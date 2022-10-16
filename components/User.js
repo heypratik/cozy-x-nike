6ef8fd23eb98078ecc49a19f98416535476a163b
Pratik Ravariya <pratikr0810@gmail.com>
2022-10-16 21:12:02 +0530

    re7

--- components/User.js
+++ components/User.js
@@ -2,26 +2,37 @@ import { FaUserCircle } from "react-icons/fa";
 import { useRouter } from "next/router";
 import styled from "styled-components";
 import { useUser } from "@auth0/nextjs-auth0";
+import React, {useState} from "react"
 
 export default function User() {
   const route = useRouter();
+
   const { user, error, isLoading } = useUser();
+  const [logoutDiv, setLogOutDiv] = useState(false)
+
+  function seeAuth() {
+    if (!user) {
+      route.push("/api/auth/login")
+    } else {
+      setLogOutDiv(prevState => !prevState)
+    }
+  }
 
   if (!user)
     return (
-      <div onClick={() => route.push("/api/auth/login")}>
+      <div className="login" onClick={seeAuth}>
         <FaUserCircle />
         <h3>Login</h3>
       </div>
     );
 
   return (
-    <Profile>
-      <img src={user.picture} alt={user.name} />
+    <Profile onClick={seeAuth}>
+      <img className={`imgPP ${logoutDiv && 'imgbord'}`} src={user.picture} alt={user.name} />
       <h3>{user.name}</h3>
-      <div>
-        <p>Logout</p>
-      </div>
+      {logoutDiv && <div className="logout">
+        <p onClick={() => route.push("/api/auth/logout")}>Logout</p>
+      </div>}
     </Profile>
   );
 }
@@ -31,5 +42,4 @@ const Profile = styled.div`
     border-radius: 50%;
     width: 1.5rem;
     height: 1.5rem;
-  }
-`;
+  }`;
