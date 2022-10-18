f67304547f4757ab6e172b2d478f909830f51812
Pratik Ravariya <pratikr0810@gmail.com>
2022-10-18 16:21:15 +0530

    re12

--- components/Products.js
+++ components/Products.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Products(props) {
   const { title, price, image, slug } = props.attributes;
-  let bannerImg = image.data.attributes.small.url;
+  let bannerImg = 'https://res.cloudinary.com/devsleqoe/image/upload/v1662886910/medium_ispa_link_shoes_ZN_622v_3cb3a24ec1.jpg'
 
   return (
     <ProductStyle
